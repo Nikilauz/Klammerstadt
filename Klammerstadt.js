487302a06181e0de3e3e4c84d01ebf8051d0981f
Moritz Vogel <moritzvogel24@gmail.com>
2025-08-20 11:43:56 +0200

    cleanup

--- Klammerstadt.js
+++ Klammerstadt.js
@@ -1,11 +1,32 @@
 const inputFeld = document.getElementById('inputBox');
 const guesses = document.getElementById('guesses');
-var levels = document.getElementById('levels');
 const puzzleTextField = document.getElementById('puzzleText');
+var levels = document.getElementById('levels');
+
+
 
 let guessesString = "";
-
 let puzzleText = "Klamm[erst]adt"; //TODO: aus Datei einlesen
+
+
+
+function readAndDisplay() {
+	guessesString = inputFeld.value + "<br />" + guessesString;
+	guesses.innerHTML = guessesString;
+	inputFeld.value = '';
+}
+
+function displayPuzzleText() {
+	let displayString = "";
+	
+	//iterate over String and highlight the "leaf"-backets.
+	let firstUnmarkedIndex = 0
+	//iterate over whole text
+	for(let i = 0; i < puzzleText.length; i++) {
+		
+	}
+	puzzleTextField.innerHTML = displayString;
+}
 
 
 inputFeld.addEventListener('keydown', function (event) {
@@ -17,35 +38,4 @@ inputFeld.addEventListener('keydown', function (event) {
 	}
 });
 
-
-
 inputFeld.focus();
-
-function readAndDisplay() {
-	guessesString = inputFeld.value + "<br />" + guessesString;
-	guesses.innerHTML = guessesString;
-	inputFeld.value = '';
-}
-
-function ksBit(phrase, solution, start, end, kids) {
-	this.phrase = phrase; // the puzzle
-	this.solution = solution; // the solution
-	this.start = start; // start and end position in parent ksBit.
-	this.end = end;
-	this.kids = kids;
-}
-
-function displayPuzzleText() {
-	let displayString = "";
-	
-	
-	//iterate over String and highlight the "leaf"-backets.
-	let firstUnmarkedIndex = 0
-	//iterate over whole text
-	for(let i = 0; i < puzzleText.length; i++) {
-		
-	}
-	
-	
-	puzzleTextField.innerHTML = displayString;
-}
