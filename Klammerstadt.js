e87ce74c64664db2c200e6d27db32b9872d9fa33
NPruen <niklas.pruen@gmail.com>
2025-08-20 11:28:15 +0200

    added Puzzle texxt field

--- Klammerstadt.js
+++ Klammerstadt.js
@@ -1,8 +1,11 @@
 const inputFeld = document.getElementById('inputBox');
 const guesses = document.getElementById('guesses');
 var levels = document.getElementById('levels');
+const puzzleTextField = document.getElementById('puzzleText');
 
 let guessesString = "";
+
+let puzzleText = "Klamm[erst]adt"; //TODO: aus Datei einlesen
 
 
 inputFeld.addEventListener('keydown', function(event) {
@@ -47,4 +50,19 @@ function ksBit(phrase, solution, start, end, kids) {
 	this.start = start; // start and end position in parent ksBit.
 	this.end = end;
 	this.kids = kids;
+}
+
+function displayPuzzleText() {
+	let displayString = "";
+	
+	
+	//iterate over String and highlight the "leaf"-backets.
+	let firstUnmarkedIndex = 0
+	//iterate over whole text
+	for(let i = 0; i < puzzleText.length; i++) {
+		
+	}
+	
+	
+	puzzleTextField.innerHTML = displayString;
 }
